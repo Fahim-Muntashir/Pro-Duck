19b43d09642df1f661736596bd57c7ca9149e8dd
Fahim Muntashir <fahimasdf07@gmail.com>
2025-07-15 10:45:49 +0600

    some customizatinn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { DM_Sans  } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const dm_Sans = DM_Sans({
   subsets: ["latin"],
+  variable: "--font-geist-sans",
 });
 
 export const metadata: Metadata = {
@@ -25,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${dm_Sans} antialiased`}
       >
         {children}
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function Home() {
   return (
-  
-  <div>
-<Button variant="cwa">Button</Button>  </div>
+    <div className="flex flex-col gap-y-4">
+      <div>
+        <Button variant={"elevated"}>I ama button</Button>
+      </div>
+      <div>
+        <Input placeholder="I loev you"></Input>
+      </div>
+      <div>
+        <Progress value={400} />
+      </div>
+
+      <div>
+        <Textarea name="" id="" value={"tello"}>
+          I ama textarea
+        </Textarea>
+      </div>
+    </div>
   );
 }
